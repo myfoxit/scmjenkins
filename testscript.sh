ee0cc1ab30cffeb73d2e715bd68ed1c586f5f45a
ahoehne <alexander.hoehne@bearingpoint.com>
2020-04-04 09:57:06 +0200

    Changing testscript to heaphalonscript

--- testscript.sh
+++ testscript.sh
@@ -2,4 +2,5 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "This is now a full wuselffffjdbudfhudfhufdd script"
+echo "This is now a full wuselffffjdbudfhudfhufhsdbubdubds
+dd script"
